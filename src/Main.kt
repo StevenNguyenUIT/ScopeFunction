20c501d5bdb25ea0b5bf939e48ce2f42d31de715
StevenNguyenUIT <143907704+StevenNguyenUIT@users.noreply.github.com>
2024-05-27 10:20:17 -0500

    simple for 5 scope in main

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
-    println("Hello World!")
+    val str = "Hello"
+    str.let {
+        println(it.length)
+    }
+    str.apply { println(this.length) }
+    str.run { println(this.length) }
+    str.also { println(it.length) }
+    with(str) { println(this.length) }
 }
---
+++ src/let/example1.kt
@@ -0,0 +1,22 @@
+package let
+//Executing a lambda on non-nullable objects: let
+//
+//Introducing an expression as a variable in local scope: let
+
+fun main() {
+    val person1 = Person("steve",32)
+    println(person1)
+    val person2 = Person("Nhinh",33).let {
+        println(it)
+    }
+    println(person2)
+    val person3 = Person("Michale",30).let {
+        it.age = 12
+        return@let it
+    }
+    println(person3)
+    println(person3.age)
+}
+
+class Person(var name: String, var age: Int)
+
